e8ab917c61629655b082ffc84359e4a54dc5e0d8
roney ruiz <roneyruizrojas@gmail.com>
2025-06-20 23:38:50 -0600

    feat(server): add apollo to express server

--- server/src/Bootstrap.ts
+++ server/src/Bootstrap.ts
@@ -1,4 +1,10 @@
+import { json } from "body-parser";
+
 import type { Express } from "express";
+import { ApolloServer } from "@apollo/server";
+
+import { expressMiddleware } from "@as-integrations/express5";
+import type { GraphQLContext } from "@Graphql/GraphQLContext";
 
 import { PingRouter } from "@Routers/PingRouter";
 
@@ -7,5 +13,13 @@ export class Bootstrap {
 
   public addRouting(): void {
     this.app.use("/api/ping", PingRouter);
+  };
+
+  public addMiddleware(): void {
+    this.app.use(json());
+  };
+
+  public addGraphQL(apollo: ApolloServer<GraphQLContext>): void {
+    this.app.use("/graphql", expressMiddleware(apollo));
   };
 };
--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,55 +1,17 @@
 import express from "express";
+import { Apollo } from "./Apollo";
 import { Bootstrap } from "./Bootstrap";
-import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@as-integrations/express5";
-
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
-interface GraphQLContext {};
-
-
-
 
 const app = express();
+
+const apollo = new Apollo();
+await apollo.start();
 
 const bootstrap = new Bootstrap(app);
 
 bootstrap.addRouting();
-
-app.use(express.json());
-
-
-
-const apollo = new ApolloServer<GraphQLContext>({ typeDefs, resolvers });
-await apollo.start();
-
-app.use("/graphql", expressMiddleware(apollo));
+bootstrap.addMiddleware();
+bootstrap.addGraphQL(apollo.instance);
 
 app.listen(3000, () => {
   console.log("the server is running!!!");
